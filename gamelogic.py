7967f94460a7fb45f66bc67e2a77a42511493cbb
Piotr Pośpiech <piootrp01@gmail.com>
2022-03-30 20:47:31 +0200

    add gamelogic.py
    add gamelogic.py with object oriented mu torere logic
    also some tests to Node class

---
+++ gamelogic.py
@@ -0,0 +1,43 @@
+class   GameBoard:
+    movecount = 0
+    def __init__(self,nodes):
+        self.nodes = nodes  # tablica węzłów
+    def move(node1,node2):
+        if(node1.is_possible_to_move(node2)):
+            node2.color = node1.color
+            node1.color = 0
+            GameBoard.movecount += 1
+    def play(self):
+        if GameBoard.movecount >= 200:
+            return ("Game over")
+        if GameBoard.movecount % 2 == 0:
+            return
+            # Ruch czarnego
+        else:
+            return
+            # Ruch białego
+# Zajebałem sie pisząc te funkcje wyżej, chyba najłatwiej będzie to zrobić po zrobieniu gui
+# W gui też byśmy zrobili jakąś formę weryfikacji czy ten żeton można ruszyć
+# Albo nie wiem inaczej to 
+# No generalnie to propozycja tylko 
+
+class   Node:
+    def __init__(self,index,color):
+        # Każdy węzeł ma numer (0 - 8) i kolor
+        self.index=index
+        self.color=color
+    def is_possible_to_move(self,node):
+        if node.color == 0: # Pole musi być puste
+            if self.index == node.index + 1 or self.index == node.index - 1: # Pole do którego chcemy przenieść żeton musi sąsiadować z naszym polem
+                return True
+            elif self.index == node.index + 7 or self.index == node.index - 7: # Szczególny przypadek
+                return True
+            elif node.index == 8 or self.index == 8:   # Albo być w środku
+                return True
+        return False
+
+
+
+
+
+        
